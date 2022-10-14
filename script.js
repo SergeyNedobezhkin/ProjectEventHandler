da3e0d58a094810e4a7ad28defd67bdd5439533e
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-14 23:02:05 +0400

    Устранил замечания к lesson11

--- script.js
+++ script.js
@@ -1,38 +1,25 @@
 'use strict';
-
 const btn = document.getElementById('btn');
 const range = document.getElementById('range');
 const rangeSpan = document.getElementById('range-span');
+const input = document.getElementById('text');
+const InputSpan = document.getElementById('text-span');
+const square = document.getElementById('square');
 const circle = document.getElementById('circle');
-const text = document.getElementById('text');
-const textSpan = document.getElementById('text-span');
 
-let textSpanColor;
-let rangeSpanVallue;
+const colorTransfer = function (event) {
+    square.style.backgroundColor = InputSpan.textContent;
+    InputSpan.textContent = event.target.value;
+};
 
+const change = function () {
+    rangeSpan.textContent = range.value + '%';
+    circle.style.width = range.value + '%';
+    circle.style.height = range.value + '%';
+};
 document.getElementById('e_btn').style.display = 'none';
-const textLogger = function (event) {
-    textSpanColor = textSpan.textContent = event.target.value;
-};
+change();
 
-btn.addEventListener("click", function () {
-    document.getElementById('square').style.backgroundColor = textSpanColor;
-});
-
-const rangeLogger = function (event) {
-    rangeSpanVallue = rangeSpan.textContent = event.target.value;
-    console.dir(event.type);
-    if (event.type === "input") {
-        circleLogger();
-    }
-};
-
-const circleLogger = function () {
-    circle.style.width = rangeSpanVallue + "%";
-    circle.style.height = rangeSpanVallue + "%";
-};
-
-text.addEventListener('input', textLogger);
-range.addEventListener('input', rangeLogger);
-circle.addEventListener('input', circleLogger);
-
+btn.addEventListener("click", colorTransfer);
+input.addEventListener("input", colorTransfer);
+range.addEventListener("input", change);
