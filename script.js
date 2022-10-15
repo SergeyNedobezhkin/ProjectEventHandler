71d1efa2de914b9802ea20a327f3840022419724
Sergey Nedobezhkin <zelenka1633@gmail.com>
2022-10-15 09:36:18 +0400

    Устранил замечания к lesson11

--- script.js
+++ script.js
@@ -3,13 +3,11 @@ const btn = document.getElementById('btn');
 const range = document.getElementById('range');
 const rangeSpan = document.getElementById('range-span');
 const input = document.getElementById('text');
-const InputSpan = document.getElementById('text-span');
 const square = document.getElementById('square');
 const circle = document.getElementById('circle');
 
-const colorTransfer = function (event) {
-    square.style.backgroundColor = InputSpan.textContent;
-    InputSpan.textContent = event.target.value;
+const colorTransfer = function () {
+    square.style.backgroundColor = input.value;
 };
 
 const change = function () {
@@ -17,9 +15,9 @@ const change = function () {
     circle.style.width = range.value + '%';
     circle.style.height = range.value + '%';
 };
+
 document.getElementById('e_btn').style.display = 'none';
 change();
 
 btn.addEventListener("click", colorTransfer);
-input.addEventListener("input", colorTransfer);
 range.addEventListener("input", change);
